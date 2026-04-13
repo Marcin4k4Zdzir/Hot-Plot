559754e3714d2513a8bbca36d93ef98ace3c2757
Marcin <pleskaczmarcin@gmail.com>
2026-04-13 19:08:18 +0200

    Changed colors of the page and improved individual articles visuals

--- article.js
+++ article.js
@@ -8,11 +8,14 @@ document.addEventListener('DOMContentLoaded', async () => {
 });
 
 function initView(articles) {
+    debugger;
     const articleId = getArticleIdFromUrl();
     document.querySelector('.article-title').innerHTML = articles[articleId].title;
     document.querySelector('.article-lead').innerHTML = articles[articleId].lead;
     document.querySelector('.article-image').src = articles[articleId].imageSource;
-    document.querySelector('.article-text').innerHTML = articles[articleId].text;
+    document.querySelector('.article-text').innerHTML = articles[articleId].text.map((paragraph) => `
+        <p>${paragraph}</p>`
+    ).join('');
 }
 
 function getArticleIdFromUrl() {
